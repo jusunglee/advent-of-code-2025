85d1d0c47e1397ae7a5e7fc6eb90772a5b42c828
juice <juice@MacBook-Air.local>
2025-12-27 21:41:53 -1000

    day 1 solved

---
+++ day1/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type dialDirection int
+
+const (
+	dialDirectionInvalid dialDirection = iota
+	dialDirectionLeft
+	dialDirectionRight
+)
+
+func (d dialDirection) String() string {
+	return [...]string{"DIAL_DIRECTION_INVALID", "DIAL_DIRECTION_LEFT", "DIAL_DIRECTION_RIGHT"}[d]
+}
+
+// L150, R1, L33, etc
+func decodeLine(line string) (dialDirection, int, error) {
+	line = strings.TrimSuffix(line, "\n")
+
+	directionStr := line[0:1]
+	direction := dialDirectionInvalid
+	switch directionStr {
+	case "L":
+		direction = dialDirectionLeft
+	case "R":
+		direction = dialDirectionRight
+	default:
+		return dialDirectionInvalid, 0, fmt.Errorf("invalid direction %s", directionStr)
+	}
+
+	number, err := strconv.Atoi(line[1:])
+	if err != nil {
+		return dialDirectionInvalid, 0, fmt.Errorf("converting line string to number %q", err)
+	}
+
+	return direction, number, nil
+}
+
+// TODO: Could be fun to implement this under a DialSolver.Solve interface
+// and implement other solutions, like using an InverseDial solution (L10 = R90 for dialMax=100)
+// to see how the solutions compare in time complexity. For now, we only implement my first solution
+// which passed the online checker.
+func mainE(fileName string, dialDefault int, dialMax int, debug bool) error {
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		return fmt.Errorf("reading file %q", err)
+	}
+
+	lines := strings.Lines(string(content))
+	dial := dialDefault
+	numZeros := 0
+	for line := range lines {
+		dialDirection, number, err := decodeLine(line)
+		if err != nil {
+			return fmt.Errorf("decoding line %q", err)
+		}
+
+		// Simplify by getting rid of any full rotations, as rotating by
+		// n results in the same ending number as rotating by n % dialMax in either direction.
+		number %= dialMax
+		oldDial := dial
+
+		switch dialDirection {
+		case dialDirectionRight:
+			dial += number
+
+			// Can still cross the threshold at most once
+			// (e.g. for dialmax 100 and dialStart 50, 50 + 60 = 110 =~= 10),
+			// Therefore, modulo by dialMax once more.
+			dial %= dialMax
+		case dialDirectionLeft:
+			dial -= number
+
+			// If the counterclockwise dialing results in crossing the threshold (0),
+			// then just add dialMax to it to invert it back into a valid dial position.
+			// e.g. dialMax=100 dialStart=50: 50 -> L160 -> L60 -> -10, -10 + 100 = 90
+			if dial < 0 {
+				dial += dialMax
+			}
+		default:
+			return fmt.Errorf("unexpected direction %s", dialDirection)
+		}
+
+		if dial == 0 {
+			numZeros += 1
+		}
+
+		if debug {
+			fmt.Printf("line %s direction %s number %d, went from %d to %d\n", line, dialDirection, number, oldDial, dial)
+		}
+	}
+
+	fmt.Printf("The dial landed on 0 %d times\n", numZeros)
+	return nil
+}
+
+func main() {
+	var dialMax, dialStart int
+	var fileName string
+	var debug bool
+
+	flag.IntVar(&dialMax, "dial-max", 100, "the number of valid dial markers starting from [0 ex) 100 means [0-99] are valid")
+	flag.IntVar(&dialStart, "dial-start", 50, "the dial marker to start the operation at")
+	flag.StringVar(&fileName, "file-name", "input.txt", "name of the file that contains the combination list, must be in working directory")
+	flag.BoolVar(&debug, "debug", false, "print out the dial before/after of each combo applied")
+
+	flag.Parse()
+
+	if err := mainE(fileName, dialMax, dialStart, debug); err != nil {
+		log.Fatalf("failed to run %q", err)
+	}
+
+	fmt.Println("success!")
+}
